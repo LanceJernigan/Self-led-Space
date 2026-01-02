1da52741d0d25453c13675a3fba894f0187cc935
LanceJernigan <lance@lancejernigan.com>
2026-01-02 01:59:40 -0500

    Add homepage hero component

---
+++ components/hero/index.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./component.module.css";
+import { useEffect, useState } from "react";
+
+const quotes = [
+	{
+		text: "One who conquers the sea today is ready to conquer the ocean tomorrow.",
+		author: "Matshona Dhliwayo",
+	},
+	{
+		text: "Small steps taken with intention can carry us to places we once thought unreachable.",
+		author: "Brené Brown",
+	},
+	{
+		text: "Out of difficulties grow miracles of courage, clarity, and compassion.",
+		author: "Anaïs Nin",
+	},
+];
+
+const Hero = () => {
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	useEffect(() => {
+		const slideInterval = setInterval(() => {
+			setCurrentSlide((prevSlide) => (prevSlide + 1) % quotes.length);
+		}, 5000);
+		return () => clearInterval(slideInterval);
+	}, []);
+
+	return (
+		<section className={styles.component}>
+			<div className={styles.wrapper}>
+				<div className={styles.content}>
+					<h1>Grow at your own pace.</h1>
+					<p>
+						A safe, nurturing space for healing and self-discovery. Together,
+						we’ll create the balance and peace you’ve been seeking.
+					</p>
+				</div>
+				<div className={styles.quote}>
+					<ul className={styles.list}>
+						{quotes.map((quote, index) => (
+							<li
+								className={styles.item}
+								key={index}
+								data-active={index === currentSlide}
+							>
+								<h2>{quote.text}</h2>
+								<h6>
+									- {quote.author}{" "}
+									<svg
+										width="30"
+										height="30"
+										viewBox="0 0 30 30"
+										className={styles.progress}
+									>
+										<circle className={styles.progressBg}></circle>
+										<circle className={styles.progressFg}></circle>
+									</svg>
+								</h6>
+							</li>
+						))}
+					</ul>
+				</div>
+				<Image
+					src="/assets/images/homepage-hero.jpg"
+					alt="Calm mountain range"
+					fill
+					priority
+					sizes="(min-width: 1340px) 1340px, 100vw"
+					className={styles.background}
+					quality="100"
+				/>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
